ba424190f4c4c73d604f054c8157ad2a2c81a9f1
Sunithabh <sunithaprasad.c@gmail.com>
2021-08-17 21:31:09 -0500

    Starter Code

---
+++ src/main/java/com/company/hellobanking/HelloBankingApplication.java
@@ -0,0 +1,13 @@
+package com.company.hellobanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloBankingApplication.class, args);
+	}
+
+}
