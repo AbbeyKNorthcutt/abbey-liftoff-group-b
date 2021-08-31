f69fa3b38006d84004ef0a47cb8bb8764102fec5
shweta2kb <shweta2kb@gmail.com>
2021-08-30 20:11:59 -0500

    Registration page

--- src/main/java/com/company/hellobanking/controllers/HelloBankingController.java
+++ src/main/java/com/company/hellobanking/controllers/HelloBankingController.java
@@ -1,5 +1,8 @@
 package com.company.hellobanking.controllers;
 
+import com.company.hellobanking.models.User;
+import com.company.hellobanking.models.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -7,10 +10,23 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class HelloBankingController {
 
-    @GetMapping
+    @Autowired
+    private UserRepository userRepo;
+    @GetMapping("")
     public String  DisplayLanding(Model model){
-
         return "index";
     }
-
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+    @PostMapping("/process_register")
+    public <BCryptPasswordEncoder> String processRegister(User user) {
+        //BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        //String encodedPassword = passwordEncoder.encode(user.getPassword());
+        //user.setPassword(encodedPassword);
+        userRepo.save(user);
+        return "register_success";
+    }
 }
--- src/main/java/com/company/hellobanking/models/data/UserRepository.java
+++ src/main/java/com/company/hellobanking/models/data/UserRepository.java
@@ -1,4 +1,13 @@
 package com.company.hellobanking.models.data;
 
-public class UserRepository {
+
+import com.company.hellobanking.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    public User findByEmail(String email);
+
+
 }
