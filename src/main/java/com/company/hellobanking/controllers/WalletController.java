964c32a16716dcbf804aadaab478f3a3a7188e9a
shweta2kb <shweta2kb@gmail.com>
2021-09-18 15:29:03 -0500

    personalwallet is done

--- src/main/java/com/company/hellobanking/controllers/WalletController.java
+++ src/main/java/com/company/hellobanking/controllers/WalletController.java
@@ -5,23 +5,46 @@ import com.company.hellobanking.models.data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 @Controller
 public class WalletController {
+    private static final String userSessionKey = "user";
     @Autowired
     UserRepository userRepository;
-    @RequestMapping(value = "{username}")
-    public String  DisplayWallet(HttpServletRequest request,Model model,@PathVariable String username){
-        System.out.println("username 11 "+username);
-        User optUser = userRepository.findByUsername(username);
-        System.out.println("username  "+optUser);
-        model.addAttribute("user", "optUser");
-
+    @RequestMapping("personal_Wallet")
+    public String  DisplayWallet(HttpServletRequest request,Model model){
+        User optUser=getUserFromSession(request.getSession());
+        String username=optUser.getUsername();
+        String fullName = optUser.getFirstName()+"  , "+optUser.getLastName();
+        String account=optUser.getAccountNumber();
+        String email=optUser.getEmail();
+        String address=optUser.getAddress();
+        int phoneNumber =optUser.getPhoneNumber();
+        model.addAttribute("username", username);
+        model.addAttribute("fullName", fullName);
+        model.addAttribute("account", account);
+        model.addAttribute("email", email);
+        model.addAttribute("phoneNumber", phoneNumber);
+        model.addAttribute("address", address);
         return "personal_Wallet";
     }
+    public User getUserFromSession(HttpSession session){
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        if (userId == null){
+            return null;
+        }
 
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isEmpty()){
+            return null;
+        }
+
+        return user.get();
+    }
 }
