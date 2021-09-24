bf4e4179fb50bfa0305b5055dfccced9aa5fda4f
Harshala <harshala2018@gmail.com>
2021-09-24 15:40:09 -0500

    Fragments and Search

---
+++ src/main/java/com/company/hellobanking/controllers/SearchController.java
@@ -0,0 +1,19 @@
+package com.company.hellobanking.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SearchController {
+    //@Autowired
+     //private atmLocatorRepository AtmLocatorRepository;
+    @RequestMapping("search")
+      public String displaySearchResults(Model model){
+      model.addAttribute("message", "Location Details");
+      return "search";
+
+  }
+}
